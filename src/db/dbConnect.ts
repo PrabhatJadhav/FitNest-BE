45d16813841ccc88a068e09f7a8538c150ebbf28
PrabhatJadhav <prabhatjadhav.exe@gmail.com>
2025-02-09 17:07:37 +0530

    db connection updated to postgresSQL

--- src/db/dbConnect.ts
+++ src/db/dbConnect.ts
@@ -1,18 +1,55 @@
-import mysql from "mysql2";
-require("dotenv").config();
+// import mysql from 'mysql2';
+// require('dotenv').config();
 
-async function dbConnect() {
-  if (process.env.DB_PUBLIC_URL) {
-    const connection = mysql.createConnection(process.env.DB_PUBLIC_URL);
+// async function dbConnect() {
+//   if (process.env.DB_URL) {
+//     const connection = mysql.createConnection(process.env.DB_URL);
 
-    // console.log("connection", connection);
+//     // console.log("connection", connection);
 
-    connection.connect((err: any) => {
-      if (err) {
-        console.log("Error connecting to the database!", err);
-        throw err;
-      }
-      console.log("Connected to the database!");
+//     connection.connect((err: any) => {
+//       if (err) {
+//         console.log('Error connecting to the database!', err);
+//         throw err;
+//       }
+//       console.log('Connected to the database!');
+//     });
+//   }
+// }
+
+// export { dbConnect };
+
+// ********************************************* New Connection *********************************************
+
+import * as fs from 'fs';
+import * as pg from 'pg';
+
+function dbConnect() {
+  if (process.env.DB_SSL_CA_PATH) {
+    const config = {
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(process.env.DB_SSL_CA_PATH).toString(),
+      },
+    };
+
+    const client = new pg.Client(config);
+
+    client.connect(function (err: any) {
+      if (err) throw err;
+      client.query('SELECT VERSION()', [], function (err: any, result: any) {
+        if (err) throw err;
+
+        console.log(result.rows[0].version);
+        client.end(function (err: any) {
+          if (err) throw err;
+        });
+      });
     });
   }
 }
