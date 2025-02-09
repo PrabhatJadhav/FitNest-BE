b68a77a701dba96ddc2ce0db0922f26f7c40ba5a
PrabhatJadhav <prabhatjadhav.exe@gmail.com>
2025-02-09 17:16:09 +0530

    v1 router defined, login etc pending

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express from "express";
-import { dbConnect } from "./db/dbConnect";
-import bodyParser from "body-parser";
-import cors from "cors";
-import { swaggerDocs } from "./swagger";
-import swaggerUi from "swagger-ui-express";
-import { testRouter } from "./routes/testRoutes";
-import "../src/cron/otpCleanupCron";
+import express from 'express';
+import { dbConnect } from './db/dbConnect';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import { swaggerDocs } from './swagger';
+import swaggerUi from 'swagger-ui-express';
+import { v1Router } from './routes/v1-routes';
+import '../src/cron/otpCleanupCron';
 
 const app = express();
 const PORT = process.env.PORT || 8080;
@@ -17,28 +17,28 @@ app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
     extended: true,
-  })
+  }),
 );
 app.use(cors());
 app.use(express.json());
 
 /************** Swagger Doc Routes **************/
 
-app.use("/api/v1/docs", swaggerUi?.serve, swaggerUi?.setup(swaggerDocs));
+app.use('/api/v1/docs', swaggerUi?.serve, swaggerUi?.setup(swaggerDocs));
 
 /************** Test, User Auth Routes **************/
 
-app.get("/", (req: any, res: any) => {
-  res.send("Hello, World abcdee!");
-});
+// app.get("/", (req: any, res: any) => {
+//   res.send("Hello, World abcdee!");
+// });
 
-app.get("/test", (req: any, res: any) => {
-  res
-    .status(200)
-    .send({ data: "Hello, this is test api welcome to port 8080" });
-});
+// app.get("/test", (req: any, res: any) => {
+//   res
+//     .status(200)
+//     .send({ data: "Hello, this is test api welcome to port 8080" });
+// });
 
-app.use("/api/v1", testRouter);
+app.use('/api/v1', v1Router);
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
