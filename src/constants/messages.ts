2ab93288b8966910a58dd72598624ab764961d69
PrabhatJadhav <prabhatjadhav.exe@gmail.com>
2025-02-17 20:32:09 +0530

    messages for api response

--- src/constants/messages.ts
+++ src/constants/messages.ts
@@ -1,5 +1,11 @@
 export const GENERAL_ERROR = 'Something Went Wrong';
+export const SESSION_EXPIRED = 'Session expired, please login again!';
+export const USER_ID_MISSING = 'User id missing!';
+export const REFRESH_TOKEN_MISSING = 'Refresh token missing!';
+export const USER_ID_NOT_FOUND = 'User not found!';
 export const INVALID_TOKEN_ERROR = 'Invalid token';
+export const INVALID_OTP_ERROR = 'Invalid or Expired OTP!';
 export const TOKEN_EXPIRED_ERROR = 'Token expired';
+export const NEW_TOKEN_GENERATION_SUCCESS = 'New token generated successfully';
 export const DECODING_TOKEN_ERROR = 'Error decoding token';
 export const NO_TOKEN_ERROR = 'Token is required to access this resource!';
